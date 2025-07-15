0387f8a2e54e5ff1b96583a1ac2edf03c3db3a17
alokmishra <alok.230101016@iiitbh.ac.in>
2025-07-16 00:25:55 +0530

    registrationEvent and details upgraded

--- controllers/event.js
+++ controllers/event.js
@@ -36,3 +36,99 @@ export const createEvent = async (req, res) => {
     return res.status(500).json({ error:"internal server error"});
   }
 };
+
+
+
+export const registerEvent = async (req, res) => {
+  try {
+const { user_id, event_id } = req.body;
+
+  
+    if (!user_id || !event_id) {
+   return res.status(400).json({ error: "user_id and event_id are required" });
+    }
+
+   
+    const [event] = await sql`
+ SELECT id, date_time, capacity FROM events WHERE id = ${event_id};
+    `;
+
+    if (!event) {
+      return res.status(404).json({ error: "Event not found" });
+    }
+
+   
+const now = moment();
+    const eventTime = moment(event.date_time);
+    if (eventTime.isBefore(now)) {
+      return res.status(400).json({ message:"cannot register for a past event"});
+    }
+
+    const [{count}] = await sql`
+      SELECT COUNT(*)::int AS count
+      FROM registrations
+      WHERE event_id = ${event_id};
+    `;
+
+    if (count >= event.capacity) {
+      return res.status(400).json({ message:"event full"});
+    }
+
+    const [found] = await sql`
+      SELECT 1 FROM registrations
+      WHERE user_id = ${user_id} AND event_id = ${event_id};
+    `;
+
+    if (found) {
+      return res.status(400).json({message:"User already registered for this event"});
+    }
+
+  
+    await sql`
+      INSERT INTO registrations(user_id, event_id)
+      VALUES (${user_id}, ${event_id});
+    `;
+
+    return res.status(201).json({message:"user registered successfully"});
+
+  } catch (error) {
+    console.error("Error registering user:", error);
+    return res.status(500).json({error:"internal server error" });
+  }
+};
+
+
+export const detailEvent = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ error: "id is required in URL params" });
+    }
+
+    const [event] = await sql`
+      SELECT * FROM events WHERE id = ${id};
+    `;
+
+    if (!event) {
+      return res.status(404).json({ message:"event not found" });
+    }
+
+   
+    const registeredUsers = await sql`
+      SELECT u.id, u.name
+      FROM registrations r
+      JOIN users u ON u.id = r.user_id
+      WHERE r.event_id = ${id};
+    `;
+
+    return res.status(200).json({
+      ...event,
+      registered_users: registeredUsers
+    });
+
+  } catch (error) {
+    console.error("Error getting event details:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- routes/event.js
+++ routes/event.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { createEvent } from '../controllers/event.js';
+import { createEvent, detailEvent, registerEvent } from '../controllers/event.js';
 
 
 const router = express.Router()
 
 router.post("/create",createEvent);
+router.post("/register",registerEvent);
+router.get("/details/:id",detailEvent);
 
 export default router;
